47926f09d7981fbd17ef955265a8d56e71589574
Justin Kwitco <Justin Kwitco>
2017-03-09 22:14:39 -0500

    Commit 1

---
+++ index.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    
+  //prompt user for purchase 
+  //calculate total price of phone (const)
+  //can keep purchasing phones
+  //max amount of money (const)
+  //list of accessories that can be purchased with the phone (const)
+  //add tax to subtotal (const)
+  //total amount
+  //prompt for remaining amount in bank
+
+  const ACCESSORY_COST = 10.00;
+  const PHONE_COST = 50.00;
+  const BANK_TALLY = 200.00; 
+  const TAX_RATE = 0.13;
+  
+  var phone = document.getElementById("phone").innerHTML = "The cost of a phone is " + "$" + PHONE_COST;
+  var accessory = document.getElementById("accessory").innerHTML = "The cost of an accessory is " + "$" + ACCESSORY_COST;
+
+  //calculate phone purchase against total in bank account 
+  function phoneCost(phoneCost, bankTally) { 
+    phoneSub = bankTally - phoneCost;
+    return phoneSub;
+  };   
+
+   //calculate accessory purchase against total in bank account 
+  function accessoryCost(accessoryCost, phoneSub) { 
+    accessorySub =  phoneSub - accessoryCost;
+    return accessorySub;    
+  };  
+
+  phoneCost(PHONE_COST, BANK_TALLY);
+  accessoryCost(ACCESSORY_COST, phoneSub);
+
+  console.log(phoneSub);
+  console.log(accessorySub);
+
+
+}; //window load end
+
+
+    
