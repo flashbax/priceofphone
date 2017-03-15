2d529a03558280f249559fe1f46e47dc7489b36e
Justin Kwitco <Justin Kwitco>
2017-03-15 00:20:57 -0400

    Commit 4

--- index.js
+++ index.js
@@ -13,35 +13,30 @@ window.onload = function() {
   const PHONE_COST = 50.00;
   const BANK_TALLY = 200.00; 
   const TAX_RATE = 0.13;
+ 
   
   var phone = document.getElementById("phone").innerHTML = "The cost of a phone is " + "$" + PHONE_COST;
   var accessory = document.getElementById("accessory").innerHTML = "The cost of an accessory is " + "$" + ACCESSORY_COST;
   //var purchaseAccessory = document.getElementById("buyAccessory").innerHTML = "Purchase Accessory";
 
-  //calculate phone purchase against total in bank account 
-  function phoneCost(phoneCost, bankTally) { 
-    phoneSub = bankTally - phoneCost;
-    return phoneSub;
-  };   
-  
-  //pass in cost of phone and the current bank account value
-  phoneCost(PHONE_COST, BANK_TALLY);
+   //pass in the cost of an accesory and the purchase of a phone
+ //document.getElementById("buyAccessory").addEventListener("click", 
+ 
+ function accessoryPurchase(){ 
 
-  //pass in the cost of an accesory and the purchase of a phone
-  document.getElementById("buyAccessory").addEventListener("click", function() {
-    console.log(ACCESSORY_COST);
-  });
+      //console.log("hello");
+  var buyAccessory = ACCESSORY_COST;
+  //var addAccessory = buyAccessory;
+  var buy = buyAccessory++;
+  console.log(buy);
+  //for (i=buyAccessory; i < buyAccessory+10; i++) {
+  //      console.log(i);
+ // }
 
-  //calculate accessory purchase 
-  // function accessoryCost(ACCESSORY_COST) { 
-  //   for (i=0; i < ACCESSORY_COST; i++) { 
-  //     return ACCESSORY_COST
-  //   } 
-  // }; 
+  };
   
-
-  //document.getElementById("bank").innerHTML = "You have " + "$" + accessorySub + " left to spend";
-  
+//document.getElementById("bank").innerHTML = "You have " + "$" + accessorySub + " left to spend";
+document.getElementById("buyAccessory").onclick = function() { accessoryPurchase() };
 
 }; //window load end
 
