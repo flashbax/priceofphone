848ea9585a060f2234ff0fec038c24072ad45a21
Justin Kwitco <Justin Kwitco>
2017-03-25 18:17:07 -0400

    Commit 9

--- index.js
+++ index.js
@@ -11,23 +11,43 @@
 
   const ACCESSORY_COST = 10.00;
   const PHONE_COST = 50.00;
-  const BANK_THRESHOLD = 200.00; 
+  const BANK_THRESHOLD = 173.59; 
   const TAX_RATE = 0.13;  
  
   var bank_balance = 300.00;
   var amount = 0;
+   
 
   function phone() { 
-      amount += 50;
+    if (amount < BANK_THRESHOLD) { 
+      amount += PHONE_COST;
       document.getElementById("subTotal").innerHTML = amount;
-      return amount;
+    } else {
+        document.getElementById("subTotal").innerHTML ="You're out of cash";
+    }
   };
 
   function accessory() { 
-      amount += 10;
+    if (amount < BANK_THRESHOLD) {
+      amount += ACCESSORY_COST;
       document.getElementById("subTotal").innerHTML = amount;
-      return amount;
+    } else { 
+        document.getElementById("subTotal").innerHTML ="You're out of cash";
+    }
   };
+
+  function total() { 
+      var tax = amount * TAX_RATE;
+      var totalAmt = tax + amount;
+      document.getElementById("total").innerHTML = totalAmt;
+      return totalAmt;
+  };
+
+  function bank() { 
+      var bank = bank_balance - total();
+      document.getElementById("bank").innerHTML = "You have $" + bank + " left in your account";
+  };
+
 
   
 
